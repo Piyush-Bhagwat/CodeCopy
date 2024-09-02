85db104b373bb2081ee98c6e18c4c006a7c7b7fc
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2024-09-02 14:28:00 +0530

    added basic homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+"use client";
+import { appContext } from "@/context/AppContext";
+import { MdLightMode, MdNightlife, MdNightlight } from "react-icons/md";
+import React, { useContext } from "react";
+import Link from "next/link";
 
 const HomePage = () => {
-  return (
-    <div>HomePagepage</div>
-  )
-}
+    const { dark, user, login, setDark } = useContext(appContext);
+    const className = {
+        btn: " rounded-full active:scale-95 transition-all",
+    };
+    return (
+        <div
+            className={`flex items-center transition-all duration-300 ${
+                dark && "dark"
+            } justify-center w-full h-[100vh] dark:bg-black dark:text-neutral-200`}
+        >
+            <div>
+                <h1 className="transition-all text-6xl font-bold mb-2">
+                    copy_code
+                </h1>
+                <h3 className="text-lg mb-5 transition-all">
+                    Store and Share Code Snippets
+                </h3>
 
-export default HomePage
+                <div className="flex gap-2 items-center">
+                    {user ? (
+                        <Link
+                            href={"/profile"}
+                            className={`${className.btn} hover:bg-indigo-400 dark:hover:bg-neutral-400 px-5 py-2 bg-indigo-500 dark:bg-neutral-300 text-white dark:text-black`}
+                        >
+                            Go To Profile
+                        </Link>
+                    ) : (
+                        <button
+                            onClick={login}
+                            className={`${className.btn} hover:bg-indigo-400 dark:hover:bg-neutral-400 px-5 py-2 bg-indigo-500 dark:bg-neutral-300 text-white dark:text-black`}
+                        >
+                            Login
+                        </button>
+                    )}
+                    <button
+                        onClick={() => setDark((p) => !p)}
+                        className={`p-2 text-2xl ${className.btn} hover:bg-indigo-400 dark:hover:bg-neutral-900 bg-indigo-300 dark:bg-neutral-800`}
+                    >
+                        {dark ? <MdNightlight /> : <MdLightMode />}
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
